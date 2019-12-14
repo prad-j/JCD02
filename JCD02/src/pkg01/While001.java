e4ef9ff3eec45529e7800d62473b71af9414995e
Pramods <Pramods@Pramod>
2019-12-15 00:17:20 +0530

    while01

---
+++ JCD02/src/pkg01/While001.java
@@ -0,0 +1,13 @@
+package pkg01;
+
+public class While001 {
+	public static void main(String args[]){
+		//this is while loop
+		int c = 0;
+		while(c<10){
+			System.out.println("This is statement number "+c);
+			c++;
+		}
+	}
+	
+}
